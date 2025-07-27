f64f9a4ae4636d041191cc4bd13ed3e0b9bded58
skrzero <Formationafec@outlook.fr>
2025-07-27 13:15:13 +0200

    r

--- schemas/gift.schema.js
+++ schemas/gift.schema.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 //TODO: completer ici le schema
 const giftSchema = Joi.object({
     title:
-    Joi.string().min(3).max(255).required(),
+    Joi.string().min(2).max(255).required(),
     description:
     Joi.string().allow('').optional(),
     price:
